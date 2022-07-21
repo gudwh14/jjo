98867620966db39d3398ee8d6d10f5c5a438c08f
gudwh14 <alxious@naver.com>
2022-07-21 23:47:04 +0900

    feat : Initailize default style

---
+++ styles/style.ts
@@ -0,0 +1,40 @@
+import styled from '@emotion/styled';
+
+export const FlexCenter = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 45px;
+`;
+
+export const Title = styled.span`
+  font-weight: normal;
+  width: 100%;
+  font-family: "Bungee Shade", cursive;
+
+  font-size: 3rem;
+  background: linear-gradient(-45deg,
+  #4bc0c8 25%,
+  #feac5e 25%,
+  #feac5e 50%,
+  #4bc0c8 50%,
+  #4bc0c8 75%,
+  #feac5e 75%,
+  #feac5e);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  background-size: 20px 20px;
+  background-position: 0 0;
+  animation: stripes 1s linear infinite;
+}
+
+@keyframes stripes {
+  100% {
+    background-position: 20px 0, 20px 0, 20px 0;
+  }
+`;
