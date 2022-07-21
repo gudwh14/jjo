d3a15f807f4958ae19fd1c24a005479be187693d
gudwh14 <alxious@naver.com>
2022-07-21 23:49:28 +0900

    feat : index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,24 @@
 import type { NextPage } from 'next';
+import Profile from '@components/Profile';
+import styled from '@emotion/styled';
+import { FlexCenter } from '../styles/style';
+import Intro from '@components/Intro';
+import Work from '@components/Work';
 
 const Home: NextPage = () => {
   return (
-    <div>
-      <span>Hello, World!</span>
-    </div>
+    <AppLayout>
+      <Profile />
+      <Intro />
+      <Work />
+    </AppLayout>
   );
 };
+
+const AppLayout = styled(FlexCenter)`
+  flex-direction: column;
+  max-width: 140rem;
+  background-color: white;
+`;
 
 export default Home;
